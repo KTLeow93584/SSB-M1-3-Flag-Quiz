6df54d81016e91f03f248db256d5117f493176d7
KTLeow93584 <leowkeantat@gmail.com>
2024-01-22 03:30:17 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const quizBuffer = {
+  "heart": "❤️",
+  "tick": "✅",
+  "cross": "❌",
+  "burger": "🍔",
+  "watermelon": "🍉",
+  "cry": "😭",
+  "sad": "😔"
+};
+
+function onLoadPage() {
+  const bufferValues = Object.values(quizBuffer);
+  document.getElementById("emojis").innerHTML = bufferValues.join("");
+}
+
+function startQuiz() {
+  const bufferKeys = Object.keys(quizBuffer);
+  const randomIndex = Math.floor(Math.random() * bufferKeys.length);
+  
+  const answer = bufferKeys[randomIndex];
+  const icon = quizBuffer[answer];
+
+  console.log("Buffer: " + Object.keys(quizBuffer) + ", Length: " + quizBuffer.length + ", Random: " + Math.random());
+  console.log("Index: " + randomIndex + ", Answer: " + answer + ", Icon: " + icon);
+  
+  const guess = prompt(`What does this ${icon} stand for?`);
+
+  if (guess.toLowerCase() === answer.toLowerCase())
+    alert("You've guessed correctly!");
+  else
+    alert(`Sorry, the correct answer was ${answer.slice(0, 1).toUpperCase() + answer.substring(1)}.`);
+}
